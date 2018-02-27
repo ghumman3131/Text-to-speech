3e057fa8fbac5762b141636cc60ee4262badde60
ghumman3131 <charanghumman@gmail.com>
2018-02-27 14:32:43 +0530

    first commit

---
+++ app/src/main/java/com/inception/texttospeech/MainActivity.java
@@ -0,0 +1,39 @@
+package com.inception.texttospeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech tto_s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextToSpeech.OnInitListener listener = new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status)
+            {
+
+            }
+        };
+
+         tto_s = new TextToSpeech(MainActivity.this , listener);
+    }
+
+    public void bol_do(View view) {
+
+
+        EditText text_et = findViewById(R.id.input_text);
+
+        String txt = text_et.getText().toString();
+
+        tto_s.speak(txt , TextToSpeech.QUEUE_FLUSH , null);
+
+
+    }
+}
