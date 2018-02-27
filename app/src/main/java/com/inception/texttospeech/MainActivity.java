34a6b53fb9b4614efd3f5e878969fa358f4db561
charan ghumman <charanghumman@gmail.com>
2018-02-27 14:38:26 +0530

    Update MainActivity.java

--- app/src/main/java/com/inception/texttospeech/MainActivity.java
+++ app/src/main/java/com/inception/texttospeech/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    // global object of TextToSpeech class
     TextToSpeech tto_s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // creating OnOnitListener interface
         TextToSpeech.OnInitListener listener = new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status)
@@ -22,12 +24,14 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        // creating object of TextToSpeech
          tto_s = new TextToSpeech(MainActivity.this , listener);
     }
 
     public void bol_do(View view) {
 
 
+        
         EditText text_et = findViewById(R.id.input_text);
 
         String txt = text_et.getText().toString();
